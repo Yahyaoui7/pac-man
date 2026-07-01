28c986ca089056401bfa1a232ad4c2a25aadfa10
Nabil Yahyaoui Idrissi <nyahyaou@c3r8p5.1337.ma>
2026-07-01 18:14:58 +0100

    i think complete parsing

--- pac_man.py
+++ pac_man.py
@@ -2,19 +2,17 @@ import sys
 from src.parsing import Parser
 
 
-def main():
-
+def main() -> int:
     if len(sys.argv) != 2:
-        print("commend run should be like python3 pac_man.py config.json")
+        print("Command should be: python3 pac_man.py config.json")
         return 1
 
     parser = Parser(sys.argv[1])
-    # try:
-    parser.parser_all()
-    # except Parser_error as e:
+    config = parser.parser_all()
 
-
+    print(config)  # only for testing now
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())

13c9f56580d7f39b7040d4757240ddbd959863eb
Mouad Mennioui <mmenniou@c2r9p8.1337.ma>
2026-07-01 17:21:53 +0100

    adding window based on level

--- src/game_loop.py
+++ src/game_loop.py
@@ -11,16 +11,48 @@
 # └── CollisionSystem
 
 from mazegenerator import MazeGenerator
+import pygame
+
+CELL_SIZE = 30
 
 
 class GameStarter:
-    def run():
-        # Create a simple 20x20 maze
-        maze_gen = MazeGenerator(
-            size=(20, 20), entry_cell=(0, 0), exit_cell=(0, 0), perfect=False, seed=0
+    def __init__(self, config):
+        self.running = True
+        self.config = config
+        self.maze = None
+        self.curr_level = config["levels"][5]
+
+    def display(self):
+
+        screen = pygame.display.set_mode(
+            (
+                self.curr_level["width"] * CELL_SIZE,
+                self.curr_level["height"] * CELL_SIZE,
+            )
+        )
+        clock = pygame.time.Clock()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            screen.fill("purple")
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+        pygame.quit()
+
+    def run(self):
+        self.maze = MazeGenerator(
+            size=(self.curr_level["width"], self.curr_level["height"]),
+            entry_cell=(0, 0),
+            exit_cell=(0, 0),
+            perfect=False,
+            seed=self.curr_level["seed"],
         )
 
-        # Get the maze structure
-        maze_grid = maze_gen.maze
-        shortest_path = maze_gen.shortest_path
-        print(maze_grid, shortest_path)
+        pygame.init()
+        self.display()
